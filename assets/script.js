a4bad6409dbf108a9d56b34646b8e193a29cacef
Logan <logankrieger317@gmail.com>
2023-10-20 09:59:09 -0500

    Back End CRUD after Reservation Error

--- assets/script.js
+++ assets/script.js
@@ -11,3 +11,16 @@ function existingUser(){
 }
 
 //new user data function - needed
+
+function handleSubmit(event) {
+    event.preventDefault();
+  
+    const data = new FormData(event.target);
+  
+    const value = data.get('email');
+  
+    console.log({ value });
+  }
+  
+  const form = document.querySelector('form');
+  form.addEventListener('submit', handleSubmit);
--- server.js
+++ server.js
@@ -2,14 +2,17 @@ const db =require('./db');
 const express = require('express');
 const mongoose = require('mongoose');
 const logger = require('morgan');
+const bodyParser = require('body-parser');
 const userController = require('./controllers/userController');
 const dogController = require('./controllers/dogController');
+// const reservationsController = require('./controllers/reservationsController');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(logger('dev'));
 app.use(express.json());
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.json({ message: 'API root' });
@@ -18,20 +21,24 @@ app.get('/', (req, res) => {
 app.get('/users', userController.getAllUsers);
 app.get('/users/:id', userController.getUserById);
 app.delete('/users/:id', userController.deleteUser);
-app.post('/users/:id', userController.updateUser);
-app.post('/users/:id', userController.createUser);
+app.post('/users', userController.updateUser);
+app.put('/users', userController.createUser);
 
 app.get('/dogs', dogController.getAllDogs);
 app.get('/dogs/:id', dogController.getDogsById);
 app.delete('/dogs/:id', dogController.deleteDog);
-app.post('/dogs/:id', dogController.updateDog);
-app.post('/dogs/:id', dogController.createDog);
+app.post('/dogs', dogController.updateDog);
+app.put('/dogs', dogController.createDog);
 
 // app.get('/reservations', reservationsController.getAllReservations);
 // app.get('/reservations/:id', reservationsController.getReservationsById);
 // app.delete('/reservations/:id', reservationsController.deleteReservations);
 // app.post('/reservations/:id', reservationsController.updateReservations);
-// app.post('/reservations/:id', reservationsController.createReservations);
+// app.put('/reservations/', reservationsController.createReservations);
 
+
+ 
+// app.post('/', function(request, response){ 
+//   console.log(request.body.data);   });    
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
